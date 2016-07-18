94b2b3dc22eb1e78bca60e8cdf7169e2022de91d
Matteo <matteo@boostco.de>
2016-07-18 09:35:15 +0200

    category vc

--- appname/CategoryViewController.swift
+++ appname/CategoryViewController.swift
@@ -6,19 +6,55 @@
 //  Copyright © 2016 boostco.de. All rights reserved.
 //
 
+import AlamofireRouter
+import DGElasticPullToRefresh_CanStartLoading
 import UIKit
 
 class CategoryViewController: UIViewController {
+    
+    @IBOutlet weak var table: UITableView?
+    
+    var apiNet: RouterProtocol?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // pull to refresh setup
+        let loadingView = DGElasticPullToRefreshLoadingViewCircle()
+        loadingView.tintColor = .whiteColor()
+        
+        guard let t = table else { return }
+        
+        t.dg_addPullToRefreshWithActionHandler({ [unowned self] in
+            
+            // TODO: update data from remote
+            
+            // update data
+            /*self.api.getRepos() { json, error in
+                if error != nil {
+                    log.error(error)
+                } else {
+                    awesomeJSON = json
+                    if let cats = json?["categories"].arrayValue {
+                        self.elements = AwesomeCategory.categories(cats).sort({ $0.title.lowercaseString < $1.title.lowercaseString })
+                        //Defaults[.categories] = self.elements
+                        self.collectionView!.dg_stopLoading()
+                    }
+                }
+                
+            }*/
+            
+            }, loadingView: loadingView)
+        t.dg_setPullToRefreshFillColor(UIColor.awesomeColor())
+        t.dg_setPullToRefreshBackgroundColor(.whiteColor())
+        
+        // perform update w/ animation delayed by 0.1 sec
+        self.performSelector(#selector(CategoryViewController.updateWithLittleDelay), withObject: nil, afterDelay: 0.1)
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
@@ -32,4 +68,26 @@ class CategoryViewController: UIViewController {
     }
     */
 
+}
+
+extension CategoryViewController {
+    
+    // MARK: - Delayed update
+    func updateWithLittleDelay() {
+        guard let t = table else { return }
+        t.dg_startLoading()
+    }
+
+}
+
+// MARK: - Table handler
+extension CategoryViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
