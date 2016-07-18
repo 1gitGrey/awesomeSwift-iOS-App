e1fc32905b27cb72a2b12ec04376d1be0acb1ff3
Matteo <matteo@boostco.de>
2016-07-18 09:31:38 +0200

    Custom color as extension

--- appname/Styles.swift
+++ appname/Styles.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
-let kAwesomeColor = UIColor(red: 247/255.0, green: 67/255.0, blue: 151/255.0, alpha: 1.0)
+// MARK: - Custom colors
+extension UIColor {
+    
+    class func awesomeColor() -> UIColor {
+        return UIColor(red: 247/255.0, green: 67/255.0, blue: 151/255.0, alpha: 1.0)
+    }
+}
 
 /*struct Style {
 
