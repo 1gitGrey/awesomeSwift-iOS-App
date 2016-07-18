6e625f532c239a6fbefb8cfc2a96800b3ec607ea
Matteo <matteo@boostco.de>
2016-07-18 16:45:40 +0200

    improved base vc

--- appname/BaseViewController.swift
+++ appname/BaseViewController.swift
@@ -6,30 +6,97 @@
 //  Copyright © 2016 boostco.de. All rights reserved.
 //
 
+import AlamofireUIManager
+import RealmSwift
+import SwiftyJSON
 import UIKit
 
 class BaseViewController: UIViewController {
 
+    // swiftlint:disable:next force_cast
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    let netManager = AlamofireUIManager.sharedInstance
+    
+    // swiftlint:disable:next force_try
+    let realm = try! Realm()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        netManager.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+// MARK: - Status Bar White
+extension BaseViewController {
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+}
+
+// MARK: - AlamofireUIManagerDelegate
+extension BaseViewController: AlamofireUIManagerDelegate{
+
+    func createSpinner() -> UIView {
+
+        let act  = UIActivityIndicatorView(frame: CGRect(x: 50, y: 50, width: 50, height: 50))
+
+        act.center = view.center
+        act.activityIndicatorViewStyle = .Gray
+
+        view.addSubview(act)
+
+        act.startAnimating()
+
+        return act
+
+    }
+
+    func closeSpinner(spinner: UIView?) {
+        
+        guard spinner != nil else { return }
+        
+        if let act = spinner as? UIActivityIndicatorView {
+            
+            act.stopAnimating()
+            act.removeFromSuperview()
+            
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func checkJson(json: JSON, showError: Bool, completionHandler: AFRequestCompletionHandler, errorHandler: AFRequestErrorHandler) {
+        
+        if let errorStr = json["error"]["message"].string { // Probably authorization required
+            
+            let error = NSError(domain: "json", code: 401, userInfo: ["info": errorStr])
+            
+            errorHandler(error)
+            
+        } else { completionHandler(json) }
+        
     }
-    */
+    
+    func manageAlertError(error: NSError?, completition: AFRequestCompletionVoid) {
+        
+        let alertController = UIAlertController(title: "Error", message: error?.description, preferredStyle: .Alert)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: { _ in
+            
+            self.netManager.closeAlert()
+            completition()
+            
+        })
+        
+        alertController.addAction(defaultAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+        
+    }
 
 }
