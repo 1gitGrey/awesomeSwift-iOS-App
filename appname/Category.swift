eaefbceb9aa8c608d57a3fc4cf59d442551dd15b
Matteo <matteo@boostco.de>
2016-08-04 20:38:40 +0200

    improved datamodel

---
+++ appname/Category.swift
@@ -0,0 +1,57 @@
+//
+//  CategoryJson.swift
+//  AwesomeSwift
+//
+//  Created by Matteo Crippa on 02/06/2016.
+//  Copyright © 2016 boostco.de. All rights reserved.
+//
+
+import SwiftyJSON
+import SwiftyJSONRealmObject
+import RealmSwift
+
+class Category: SwiftyJSONRealmObject {
+
+    dynamic var id = ""
+    dynamic var title = ""
+    dynamic var descr = ""
+    dynamic var parent = ""
+    var source: RepositoryType?
+    let repositories = LinkingObjects(fromType: Repository.self, property: "category")
+
+    override static func primaryKey() -> String? {
+
+        return "id"
+    }
+
+    convenience required init(json: JSON) {
+
+        self.init()
+        
+        mapping(fromJson: json)
+
+    }
+    
+    convenience required init(withJson json: JSON, forRepository repo: RepositoryType) {
+        self.init()
+        
+        mapping(fromJson: json)
+        
+        source = repo
+    }
+    
+    func mapping(fromJson json: JSON) {
+        
+        id = json["id"].stringValue
+        title = json["title"].stringValue
+        
+        if json["description"].stringValue.characters.count > 0 {
+            descr = json["description"].stringValue
+        }
+        
+        if json["parent"].stringValue.characters.count > 0 {
+            parent = json["parent"].stringValue
+        }
+    }
+    
+}
