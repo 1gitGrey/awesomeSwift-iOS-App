0727f57b125ceeae1304d3c5abd3dea9e645cdf7
Matteo <matteo@boostco.de>
2016-08-04 11:57:34 +0200

    listed

--- appname/CellManager.swift
+++ appname/CellManager.swift
@@ -9,30 +9,29 @@
 import UITableViewCellManager
 
 enum CellManager: UITableViewCellStructureDataSource {
-    
+
     case creditsCell
 
     case repositoryCell
 
     case categoryCell
 
-    
+
     var cellStructure: UITableViewCellStructure {
         
         switch self {
             
         case .creditsCell:
             return UITableViewCellStructure(type: CreditsCell(), identifier: "CreditsCell", nib: "CreditsCell")
-            
-            
+
+
         case .repositoryCell:
             return UITableViewCellStructure(type: RepositoryCell(), identifier: "RepositoryCell", nib: "RepositoryCell")
-            
+
         case .categoryCell:
             return UITableViewCellStructure(type: CategoryCell(), identifier: "CategoryCell", nib: "CategoryCell")
         }
-        
+
     }
     
 }
-
