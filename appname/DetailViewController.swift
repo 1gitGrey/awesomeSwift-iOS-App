718dbb10233ceab0036d3db220a3c6165da7362c
Matteo <matteo@boostco.de>
2016-08-04 16:00:28 +0200

    add detail vc

---
+++ appname/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  AwesomeSwift
+//
+//  Created by Matteo Crippa on 8/4/16.
+//  Copyright © 2016 boostco.de. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
